9e98fa28badfefa2a16cae131bce9c7121366f9f
Scott <scott-f-1@hotmail.com>
2018-09-07 18:01:45 -0500

    LN 1 changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: '/checklists', pathMatch: 'full' },
+  { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
+  { path: 'checklists', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'checklists/:id', loadChildren: './checklist/checklist.module#ChecklistPageModule' },
 ];
 
 @NgModule({
